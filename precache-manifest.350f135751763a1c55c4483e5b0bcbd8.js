bc43b47448ed15accea3c17e3b8908b7c0ce24bf
SinthooranR <srparamu0716@gmail.com>
2020-08-28 16:18:05 -0400

    Updates

---
+++ precache-manifest.350f135751763a1c55c4483e5b0bcbd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0e97e47908a90c2981c4fc36bc399f",
+    "url": "/SinthooranR-react/index.html"
+  },
+  {
+    "revision": "81a6135eaf8cd0b58bab",
+    "url": "/SinthooranR-react/static/css/main.40e2643e.chunk.css"
+  },
+  {
+    "revision": "b1096be99a14f25bc4c0",
+    "url": "/SinthooranR-react/static/js/2.3ef3e61b.chunk.js"
+  },
+  {
+    "revision": "6ff3be09b66a6bae4c4000c31aff339c",
+    "url": "/SinthooranR-react/static/js/2.3ef3e61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a6135eaf8cd0b58bab",
+    "url": "/SinthooranR-react/static/js/main.c52efb21.chunk.js"
+  },
+  {
+    "revision": "c8a005c2ce63fb4fefd3",
+    "url": "/SinthooranR-react/static/js/runtime-main.eac657b6.js"
+  },
+  {
+    "revision": "a29e8eb9158ec82a7277e316294efb1d",
+    "url": "/SinthooranR-react/static/media/Sinthooran_Ravinathan_Resume.a29e8eb9.pdf"
+  },
+  {
+    "revision": "66512055ea1b7ff0d5b65484144561ba",
+    "url": "/SinthooranR-react/static/media/me.66512055.png"
+  },
+  {
+    "revision": "50e70c878dc80e5be3ebf7179044d0e1",
+    "url": "/SinthooranR-react/static/media/react.50e70c87.gif"
+  }
+]);

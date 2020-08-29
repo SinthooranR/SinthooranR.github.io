4b35898c1160380ed42d3c914f0c71169df9d5c9
SinthooranR <srparamu0716@gmail.com>
2020-08-28 22:18:13 -0400

    fixed some media queries

--- src/components/MainBody/AboutMe/AboutMe.js
+++ src/components/MainBody/AboutMe/AboutMe.js
@@ -16,7 +16,7 @@ const aboutMe = (props) => (
                         <p>Graduated from the University of Ontario Institute of Technology(UOIT) as of May 2020 in Computer Science and aspire to work as a Web/Mobile Developer</p>
                         <span>
                                 <h2>Interests:</h2>
-                                <ul>
+                                <ul className={classes.Interests}>
                                         <li>Interested in learning various languages to help experience the worlds of Web and Mobile development</li>
                                         <li>Making use of various databases such as MongoDB, Postgres and SQLite</li>
                                         <li>Designing Pages meeting the indviduals criteria</li>
